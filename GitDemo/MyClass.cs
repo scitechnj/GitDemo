e94b59ab6cc4b3eb49b2b7f4a4b069b611ce3262
Shaya Cohen <shayac1@gmail.com>
2014-02-03 20:55:18 -0500

    deleted seth's stuff

--- GitDemo/MyClass.cs
+++ GitDemo/MyClass.cs
@@ -14,12 +14,7 @@ namespace GitDemo
 	//hello
         private readonly int _x;
 
-        public static void QuickEscalation()
-        {
-            Console.WriteLine("Yedid doesn't like this method!!");
-            Console.WriteLine("Humanity does not ask us to be happy. It merely asks us to be brilliant on its behalf");
-
-        }
+        
 
         public MyClass(int x)
         {
@@ -40,5 +35,7 @@ namespace GitDemo
         {
             DeleteSethsChanges();
         }
+
+
     }
 }
