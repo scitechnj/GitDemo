aa1bf3ab89eea49c9cff72d12c4e7eaf3c76ec37
Seth Pollack <teampollack@gmail.com>
2014-02-03 20:51:09 -0500

    for yedid

--- GitDemo/MyClass.cs
+++ GitDemo/MyClass.cs
@@ -16,6 +16,8 @@ namespace GitDemo
         public static void QuickEscalation()
         {
             Console.WriteLine("Yedid doesn't like this method!!");
+            Console.WriteLine("Humanity does not ask us to be happy. It merely asks us to be brilliant on its behalf");
+
         }
 
         public MyClass(int x)
