c1031b9b0ae6f14fc4807834a7b0c9faa8399372
Avrumi Friedman <datmonkey2002@yahoo.com>
2014-02-03 21:07:52 -0500

    Cleaned up and added google html

--- GitDemo/MyClass.cs
+++ GitDemo/MyClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,52 +9,15 @@ namespace GitDemo
 {
     public class MyClass
     {
-        //this is a comment
-
-	//really?
-        // why did you remove it?
-
-        <div class="row">
-    <div class="panel panel-info form-group">
-        <div class="yedid sux">
-            <div class="panel-heading"><h3>COMPANY</h3></div>
-            <div class="panel-body">
-                <div class="col-sm-6 col-md-4">
-                    <label for="CoName">Name</label>
-                    <input type="text" class="form-control" name="CoName" value="@company.CoName" disabled/>
-                </div>
-                <div class="col-sm-6 col-md-4">
-                    <label for="CoDateAdded">Date Added</label>
-                    
-                </div>
-                <div class="col-sm-6 col-md-4">
-                    <label for="CoStreet">Street</label>
-                    <input type="text" class="form-control" name="CoStreet" value="@company.CoStreet" disabled />
-                </div>
-    </div>
-
-	//moar stuf
-	//finally a conflict
-	//i just wwant a lil conflict
         private readonly int _x;
 
-
-        // if i was shaya i would have deleted but i didnt have the guts...
-        //public static void QuickEscalation()
-        //{
-        //    Console.WriteLine("Yedid doesn't like this method!!");
-        //}
-
-        //public MyClass(int x)
-        //{
-        //    _x = x;
-        //}
-
-        //public void PrintX()
-        //{
-        //    Console.WriteLine(_x);
+        public void GetGoogleHtml()
+        {
+            using (var webclient = new WebClient())
+            {
+                Console.WriteLine(webclient.DownloadString("http://www.google.com"));
+            }
         }
-
 
     }
 }
